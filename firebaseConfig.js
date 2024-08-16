e489b55665425d5d09845a003eaa2760f3856edf
PRASHANT-tech870 <56446798+PRASHANT-tech870@users.noreply.github.com>
2024-08-16 16:10:17 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('/home/vboxuser/Downloads/EL/server1/serviceAccountKey.json');
+const serviceAccount = require('/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
