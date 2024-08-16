154288d63d5d0fcdafcc1900b329c9bfa27a93c1
PRASHANT-tech870 <56446798+PRASHANT-tech870@users.noreply.github.com>
2024-08-16 16:10:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, db, firestore
 
 # Firebase setup
-cred = credentials.Certificate('/home/vboxuser/Downloads/EL/server1/serviceAccountKey.json')
+cred = credentials.Certificate('/serviceAccountKey.json')
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
